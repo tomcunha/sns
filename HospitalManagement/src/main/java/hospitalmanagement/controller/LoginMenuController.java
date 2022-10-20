c729e0ef022e1afab5446d8d317c434da166b9e8
Frederico Castro Lopes <fredlopes@deloitte.pt>
2022-10-20 16:07:56 +0100

    more scenes

--- HospitalManagement/src/main/java/hospitalmanagement/controller/LoginMenuController.java
+++ HospitalManagement/src/main/java/hospitalmanagement/controller/LoginMenuController.java
@@ -2,8 +2,12 @@ package hospitalmanagement.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class LoginMenuController extends SceneController{
 
@@ -11,7 +15,17 @@ public class LoginMenuController extends SceneController{
     Button signInButton;
 
     @FXML
+    TextField usernamefield;
+
+    @FXML
+    PasswordField passwordfield;
+
+    @FXML
+    Text warning;
+
+    @FXML
     public void setSignInButton() throws IOException {
         setScreen(signInButton,"AdminMenuScene.fxml");
+        warning.setText("Credentials are wrong!");
     }
 }

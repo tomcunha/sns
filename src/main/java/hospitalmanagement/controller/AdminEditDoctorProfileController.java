5971b32b8774f1539e84c3800eac2fba823c1878
Frederico Castro Lopes <fredlopes@deloitte.pt>
2022-10-24 15:25:49 +0100

    more edits

--- src/main/java/hospitalmanagement/controller/AdminEditDoctorProfileController.java
+++ src/main/java/hospitalmanagement/controller/AdminEditDoctorProfileController.java
@@ -2,13 +2,25 @@ package hospitalmanagement.controller;
 
 import hospitalmanagement.Database;
 import hospitalmanagement.Information;
+import hospitalmanagement.model.medicalLists.Hospital;
 import hospitalmanagement.model.people.Doctor;
+import hospitalmanagement.utility.SpecialitiesUtil;
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.*;
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.util.Optional;
 
 public class AdminEditDoctorProfileController {
     private static String medicalLicense;
 
+    @FXML
+    GridPane editDoctorScene;
     @FXML
     TextField nameInput;
     @FXML
@@ -38,7 +50,6 @@ public class AdminEditDoctorProfileController {
             buttonEdit.setText("Save");
             buttonDelete.setText("Cancel");
         } else if (buttonEdit.getText().equals("Save")) {
-
             Database.modifyTable(
                     "UPDATE Persons SET " +
                                 "name = '" + nameInput.getText() +
@@ -49,19 +60,21 @@ public class AdminEditDoctorProfileController {
                                 "', email = '" + emailInput.getText() +
                              "' WHERE person_id = " + getPersonID());
             Database.modifyTable(
-                    "UPDATE Doctors SET hospital_id =");
+                    "UPDATE Doctors SET hospital_id = '" + getHospital().getId() + "' WHERE medicalLicence = " + medicalLicense);
             disableAll();
             buttonEdit.setText("Edit");
+            buttonEdit.setText("Delete");
         }
     }
 
     public void deletePerson(){
         if(buttonDelete.getText().equals("Delete")){
-
+            delete();
         }else if(buttonDelete.getText().equals("Cancel")){
             disableAll();
             setInputs();
             buttonDelete.setText("Delete");
+            buttonEdit.setText("Edit");
         }
     }
 
@@ -102,8 +115,8 @@ public class AdminEditDoctorProfileController {
                 datePicker.setValue(doctor.getBirthDate());
                 medicalLicenseInput.setText(medicalLicense);
                 phoneNumberInput.setText(doctor.getContact().getPhoneNumber());
-                specialityDropdown.setValue(doctor.getMedicalLicense());
-                hospitalDropdown.setValue(doctor.getWorkingHospital());
+                specialityDropdown.setValue(doctor.getSpecialty().toString());
+                hospitalDropdown.setValue(doctor.getWorkingHospital().getName());
                 emailInput.setText(doctor.getContact().getEmail());
                 addressInput.setText(doctor.getContact().getAddress());
             }
@@ -118,4 +131,61 @@ public class AdminEditDoctorProfileController {
         }
         return 0;
     }
+
+    private Hospital getHospital(){
+        for (Hospital hospital:Information.getHospitals()){
+            if (hospital.getName().equals(hospitalDropdown.getValue())){
+                return hospital;
+            }
+        }
+        return null;
+    }
+
+    private void delete(){
+            Stage backStage = (Stage) editDoctorScene.getScene().getWindow();
+            BoxBlur blur = new BoxBlur(5, 5, 5);
+            editDoctorScene.setEffect(blur);
+
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.initModality(Modality.APPLICATION_MODAL);
+            alert.initOwner(backStage);
+            //alert.setTitle("Confirmation Dialog with Custom Actions");
+            alert.setHeaderText("Are you sure that you want to delete this Doctor?\nLicense nº " + medicalLicense + "?");
+            alert.setContentText("Choose your option.");
+
+            ButtonType buttonTypeYes = new ButtonType("YES");
+            ButtonType buttonTypeCancel = new ButtonType("CANCEL", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+            alert.getButtonTypes().setAll(buttonTypeYes, buttonTypeCancel);
+
+
+            Optional<ButtonType> yesOrCancelButton = alert.showAndWait();
+            if (yesOrCancelButton.get() == buttonTypeYes) {
+
+                Dialog<String> dialog = new Dialog<>();
+                dialog.initOwner(backStage);
+                dialog.setTitle("Delete Doctor Info");
+                dialog.setHeaderText("Are you quite sure? This will delete all data present in the programme.");
+                dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK, ButtonType.CANCEL);
+
+
+                PasswordField pwd = new PasswordField();
+                HBox content = new HBox();
+                content.setAlignment(Pos.CENTER_LEFT);
+                content.setSpacing(10);
+                content.getChildren().addAll(new Label("Please enter your password to confirm:"), pwd);
+                dialog.getDialogPane().setContent(content);
+                dialog.setResultConverter(dialogButton -> {
+                    if (dialogButton == ButtonType.OK) {
+                        return pwd.getText();
+                    }
+                    return null;
+                });
+                Optional<String> result = dialog.showAndWait();
+                if (result.isPresent()) {
+                    System.out.println(result.get());
+                }
+                editDoctorScene.setEffect(null);
+            } else {editDoctorScene.setEffect(null);}
+        }
 }

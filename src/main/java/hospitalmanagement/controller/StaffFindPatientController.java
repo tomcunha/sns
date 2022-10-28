524883d2bd3c283c5f2cd9ef0a4501c1782a1e32
marinsantos <marinsantos@deloitte.pt>
2022-10-28 15:56:33 +0100

    update

--- src/main/java/hospitalmanagement/controller/StaffFindPatientController.java
+++ src/main/java/hospitalmanagement/controller/StaffFindPatientController.java
@@ -1,31 +1,38 @@
 package hospitalmanagement.controller;
 
+import hospitalmanagement.Database;
 import hospitalmanagement.Information;
 import hospitalmanagement.model.people.Patient;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StaffFindPatientController extends SceneController {
     @FXML
-    private Button buttonAddPatient, buttonEdit, buttonDelete, buttonMainMenu, buttonPower;
-
+    private Button buttonAddPatient, buttonEdit, buttonMainMenu, buttonPower,buttonYesPopUp;
     @FXML
     private TextField ccTextField;
-
     @FXML
     private Label nrLabel;
+    @FXML
+    private GridPane patientGrid, editPatientScene, popUp;
+    @FXML
+    HBox firstTextPopUp, secondtTextPopUp;
 
     @FXML
-    private GridPane patientGrid;
+    PasswordField passwordInputpopUp;
 
     @FXML
-    private Text namePatientText, birthDatePatientText, identityNumberPatientText, nameInsuranceText;
+    private Text namePatientText, birthDatePatientText, identityNumberPatientText, nameInsuranceText, popUpText;
 
     @FXML
     public void setNewPatient() throws IOException {
@@ -87,5 +94,64 @@ public class StaffFindPatientController extends SceneController {
             throw new RuntimeException(e);
         }
     }
+
+
+    public void deletePerson() {
+        popUp.setVisible(true);
+        popUpText.setText(nameInsuranceText.getText());
+        editPatientScene.setStyle(" -fx-background-color: rgb(192,192,192);opacity:0.8;");
+        patientGrid.setStyle(" -fx-background-color: rgb(192,192,192);opacity:0.8;");
+    }
+
+    public void confirmDelete(){
+        if (buttonYesPopUp.getText().equals("Yes")){
+            firstTextPopUp.setVisible(false);
+            secondtTextPopUp.setVisible(true);
+            passwordInputpopUp.setStyle("-fx-effect: none");
+            buttonYesPopUp.setText("OK");
+        } else if (buttonYesPopUp.getText().equals("OK")) {
+            if (verifyPassword(passwordInputpopUp.getText())) {
+                //Database.modifyTable("DELETE FROM Patients WHERE patient_cc = '" + .... + "'");
+
+                try {
+                    setScreen(buttonYesPopUp, "StaffMenuScene.fxml");
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            } else {
+                passwordInputpopUp.setStyle("-fx-effect: dropshadow( one-pass-box, red, 15,0,0,0)");
+                passwordInputpopUp.setText("");
+            }
+
+
+        }
+
+    }
+
+    public void cancel(){
+        popUp.setVisible(false);
+        firstTextPopUp.setVisible(true);
+        secondtTextPopUp.setVisible(false);
+        editPatientScene.setStyle(" -fx-background-color: default");
+        patientGrid.setStyle("-fx-background-color: default");
+    }
+
+    public boolean verifyPassword(String insertedPass) {
+
+        int employee_id = LoginMenuController.getEmployee_id();
+
+        ResultSet rs = Database.queryTable("SELECT (password) FROM Employees WHERE employee_id=" + employee_id);
+
+        try {
+            if (rs.next()) {
+                return insertedPass.equals(rs.getString("password"));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+
 
 }

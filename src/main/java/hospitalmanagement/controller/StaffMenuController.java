5f34d14affa9403e10872b8d2933e33c2c90465e
tomcunha <tocunha@deloitte.pt>
2022-10-28 14:21:32 +0100

    New appointment select patient

--- src/main/java/hospitalmanagement/controller/StaffMenuController.java
+++ src/main/java/hospitalmanagement/controller/StaffMenuController.java
@@ -5,20 +5,39 @@ import javafx.scene.control.Button;
 
 import java.io.IOException;
 
-public class StaffMenuController extends SceneController{
+public class StaffMenuController extends SceneController {
 
     @FXML
     Button buttonPower;
     @FXML
-    Button patientsManagementButton;
+    Button patientsManagementButton, newAppointmentButton;
 
     @FXML
-    public void setButtonPower() throws IOException {
-        setScreen(buttonPower, "LoginScene.fxml");
+    public void setButtonPower() {
+        try {
+            setScreen(buttonPower, "LoginScene.fxml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
-    public void setPatientsManagement() throws IOException {
-        setScreen(patientsManagementButton, "StaffFindPatientScene.fxml");
+    public void setPatientsManagement() {
+        try {
+            setScreen(patientsManagementButton, "StaffFindPatientScene.fxml");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    public void newAppointment() {
+        try {
+            setScreen(newAppointmentButton, "StaffFindPatientScene.fxml");
+            StaffFindPatientController findPatient = getFXML().getController();
+            findPatient.selectAppointment();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
